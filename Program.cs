bc542816d3da89beb2dcb5d1ae5aa0283a9b4f6f
RuanMSantos <ruancauancauan@gmail.com>
2025-02-21 15:39:13 -0300

    Finaliza aplicação

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿int numero = 0;
+Contador();
+
+void Contador(){
+    #pragma warning disable CA1416 
+    numero++;
+    Thread.Sleep(500);
+    Console.ForegroundColor = (ConsoleColor)numero;
+    Console.Write(numero + " ");
+    Console.Beep(1000, 100);
+    if (numero < 10){
+        Contador();
+        return;
+    }
+    Console.WriteLine();
+    Thread.Sleep(500);
+    Console.ForegroundColor = (ConsoleColor)(++numero);
+    Console.WriteLine("Saindo");
+    Console.Beep(1000, 100);
+    Console.Beep(1000, 100);
+    Console.Beep(1000, 100);
+    Console.ResetColor();
+    #pragma warning restore CA1416
+}
